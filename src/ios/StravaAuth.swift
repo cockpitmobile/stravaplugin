9bf41cc1aa3645b07797bb4187cdef9dbfdb76a5
Clay Burdelik <betagole@wisc.edu>
2020-04-08 14:35:46 -0500

    Swift file for Strava plugin

--- src/ios/StravaAuth.swift
+++ src/ios/StravaAuth.swift
@@ -1,30 +1,30 @@
-@obj(StravaAuth) class StravaAuth: CDVPlugin {
-    func authorize(command: CDVInvokedUrlCommand) {
+import Foundation
+
+@objc(StravaAuth) class StravaAuth: CDVPlugin {
+    @objc(authorize:) func authorize(command: CDVInvokedUrlCommand) {
         let state = command.arguments[0]
         let callbackUri = command.arguments[1]
 
         let appOAuthUrlStravaScheme = URL(string: "strava://oauth/mobile/authorize?client_id=45576&redirect_uri=\(callbackUri)&response_type=code&approval_prompt=auto&scope=read_all&state=\(state)")!
-
-        let webOAuthUrl = URL(string: "https://www.strava.com/oauth/mobile/authorize?client_id=45576&redirect_uri=\(callbackUri)&response_type=code&approval_prompt=auto&scope=read_all&state=\(state)")!
         
-        if UIApplication.shared.canOpenURL(appOAuthUrlstravaScheme) {
-            UIApplication.shared.open(appOAuthUrlstravaScheme, options: [:])
+        if UIApplication.shared.canOpenURL(appOAuthUrlStravaScheme) {
+            UIApplication.shared.open(appOAuthUrlStravaScheme, options: [:])
+            let pluginResult = CDVPluginResult(
+                status: CDVCommandStatus_OK
+            )
+            self.commandDelegate!.send(
+                pluginResult,
+                callbackId: command.callbackId
+            )
         } else {
-            authSession = SFAuthenticationSession(url: webOAuthUrl, callbackURLScheme: "com.CockpitMobile.VirtualRace://") { url, error in
-
-            }
-
-            authSession?.start()
+            let pluginResult = CDVPluginResult(
+                status: CDVCommandStatus_ERROR
+            )
+            
+            self.commandDelegate!.send(
+                pluginResult,
+                callbackId: command.callbackId
+            )
         }
-        
-        let pluginResult = CDVPluginResult(
-            status: CDVCommandStatus_OK
-        )
-        
-        self.commandDelegate!.sendPluginResult(
-        pluginResult,
-        callbackId: command.callbackId
-        )
-
     }
 }
